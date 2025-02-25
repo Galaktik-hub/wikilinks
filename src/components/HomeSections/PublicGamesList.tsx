83d5d687b84fbca18d560c28d506aefb402e8bf0
JulienS-Code <julien.synaeve@gmail.com>
2025-02-25 19:16:24 +0100

    WIK-4: Page d'accueil - Added react-responsive dependency - Created CopySVG as a react object - Added Public Game Section with PublicGameCard and dynamic PlayerCountBadge bg-color

--- src/components/HomeSections/PublicGamesList.tsx
+++ src/components/HomeSections/PublicGamesList.tsx
@@ -1,21 +1,21 @@
 "use client";
 import React from "react";
-import { PublicGameCard } from "./PublicGameCard.tsx";
+import { PublicGameCard } from "../Cards/PublicGameCard.tsx";
 
 export const PublicGamesList: React.FC = () => {
     const games = [
         { hostName: "Pierre", playerCount: 2, maxPlayers: 6, gameCode: "594339" },
-        { hostName: "Pierre", playerCount: 5, maxPlayers: 6, gameCode: "594339" },
-        { hostName: "Pierre", playerCount: 5, maxPlayers: 6, gameCode: "594339" },
-        { hostName: "Pierre", playerCount: 2, maxPlayers: 6, gameCode: "594339" },
+        { hostName: "Marie", playerCount: 5, maxPlayers: 6, gameCode: "594339" },
+        { hostName: "Jean", playerCount: 5, maxPlayers: 6, gameCode: "594339" },
+        { hostName: "Léa", playerCount: 2, maxPlayers: 6, gameCode: "594339" },
     ];
 
     return (
-        <section className="mt-6 w-full">
+        <section className="flex flex-col items-center mt-6 w-full">
             <h2 className="text-lg font-bold leading-none text-white">
                 Parties publiques
             </h2>
-            <div className="flex flex-col gap-5 mt-5">
+            <div className="flex flex-wrap gap-5 justify-center items-start mt-5 w-full max-md:max-w-full">
                 {games.map((game, index) => (
                     <PublicGameCard key={index} {...game} />
                 ))}
