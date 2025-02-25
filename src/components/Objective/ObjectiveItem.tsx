b3a3bb2d61007fe004cb38a1f899b0fc4a87aeb1
JulienS-Code <julien.synaeve@gmail.com>
2025-02-25 23:22:38 +0100

    WIK-5: Page d'attente/jeu - Added generic Container object (related to container in figma) - UpSVG object - Dynamic component objective added

---
+++ src/components/Objective/ObjectiveItem.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import CheckCircle from "../Check/CheckCircle.tsx";
+
+interface ObjectiveItemProps {
+    text: string;
+    isReached: boolean;
+}
+
+const ObjectiveItem: React.FC<ObjectiveItemProps> = ({
+        text,
+        isReached,
+    }) => {
+    return (
+        <article
+            className={`flex gap-2.5 items-center p-2 w-full rounded min-h-10 ${
+                isReached ? "bg-gray-900" : "bg-gray-700"
+            }`}
+        >
+            <CheckCircle checked={isReached} />
+            <p className="gap-2.5 self-stretch py-1 my-auto">
+                {text}
+            </p>
+        </article>
+    );
+};
+
+export default ObjectiveItem;
---
+++ src/components/Objective/ObjectivePanel.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import * as React from "react";
+import { useState } from "react";
+import ObjectiveItem from "./ObjectiveItem.tsx";
+import UpSVG from "../../assets/UpSVG.tsx";
+import Container from "../Container.tsx";
+
+interface Objective {
+    id: string;
+    text: string;
+    isReached: boolean;
+}
+
+const ObjectivesPanel: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const togglePanel = () => {
+        setIsOpen(!isOpen);
+    };
+
+    // Sample objectives data
+    const objectives: Objective[] = [
+        {
+            id: "obj1",
+            text: "Title of the page reached",
+            isReached: true,
+        },
+        {
+            id: "obj2",
+            text: "Page title not reached",
+            isReached: false,
+        },
+        {
+            id: "obj3",
+            text: "Page title not reached",
+            isReached: false,
+        },
+    ];
+
+    return (
+        <Container className="flex flex-col justify-center w-[360px]">
+            <div className="flex gap-10 justify-between w-full">
+                <button
+                    onClick={togglePanel}
+                    aria-expanded={isOpen}
+                    aria-controls="objectives-content"
+                    className="flex justify-between w-full"
+                >
+                    <h2
+                        className="gap-2.5 self-stretch py-1 text-lg font-bold leading-none text-sky-500 whitespace-nowrap w-[95px]"
+                        style={{ textShadow: "0px 0px 14px #0ea5e9"}}
+                    >
+                        Objectifs
+                    </h2>
+                    <span
+                        className={`flex gap-2.5 min-h-[25px] transform ${isOpen ? "rotate-180" : "rotate-0"} transition-transform duration-[300ms] ease-in-out `}
+                    >
+                        <UpSVG />
+                    </span>
+                </button>
+            </div>
+
+            {isOpen && (
+                <div
+                    id="objectives-content"
+                    className="flex flex-col items-center gap-2 self-center mt-2 w-full text-base text-white"
+                >
+                    {objectives.map((objective) => (
+                        <ObjectiveItem
+                            key={objective.id}
+                            text={objective.text}
+                            isReached={objective.isReached}
+                        />
+                    ))}
+                </div>
+            )}
+        </Container>
+    );
+};
+
+export default ObjectivesPanel;
