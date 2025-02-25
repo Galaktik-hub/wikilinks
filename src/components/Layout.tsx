9d2a2d7f4d5b3a7982d864f0e43519421e39afa2
JulienS-Code <julien.synaeve@gmail.com>
2025-02-25 19:21:59 +0100

    WIK-4: Page d'accueil - Css optimisation on Layout aside using useMediaQuery

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {useMediaQuery} from "react-responsive";
 
 interface LayoutProps {
     header?: React.ReactNode;
@@ -6,22 +7,39 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ header, children }) => {
-    return (
-        <div className="min-h-screen flex flex-col md:flex-row">
-            <aside className="hidden md:flex md:w-1/4 bg-gray-600">
 
-            </aside>
-            <main className="bg-primary md:w-1/2 flex flex-col">
-                <div className="flex flex-col flex-1">
-                    {header}
-                    {children}
-                </div>
-            </main>
-            <aside className="hidden md:flex md:w-1/4 bg bg-gray-600">
+    const isMobile = useMediaQuery({maxWidth: 767});
+    if (isMobile) {
+        return (
+            <div className="min-h-screen flex flex-col md:flex-row">
 
-            </aside>
-        </div>
-    );
+                <main className="bg-primary md:w-1/2 flex flex-col">
+                    <div className="flex flex-col flex-1">
+                        {header}
+                        {children}
+                    </div>
+                </main>
+            </div>
+        )
+    } else {
+        return (
+            <div className="min-h-screen flex flex-col md:flex-row">
+
+                <aside className="flex w-1/4 bg-gray-600">
+
+                </aside>
+                <main className="bg-primary w-1/2 flex flex-col">
+                    <div className="flex flex-col flex-1">
+                        {header}
+                        {children}
+                    </div>
+                </main>
+                <aside className="flex w-1/4 bg bg-gray-600">
+
+                </aside>
+            </div>
+        )
+    }
 };
 
 export default Layout;
