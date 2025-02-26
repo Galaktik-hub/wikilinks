9e7d87993b8dccc919e3e6ec701d3fa438ec7a6f
Kirushield <elankeethan@edu.univ-eiffel.fr>
2025-02-26 20:28:50 +0100

    [+] Update logo

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -4,8 +4,8 @@ import logo from '../../assets/logo.png';
 const Logo: React.FC = () => {
     return (
         <div className={"flex items-center justify-center leading-none"}>
-            <img src={logo} alt="W" className={"h-[50px] w-auto -mb-2"} />
-            <span className={"text-base self-end text-white font-medium font-martian-mono -ml-2"}>ikilinks</span>
+            <img src={logo} alt="W" className={"h-[60px] w-auto -mb-2"} />
+            <span className={"text-lg self-end text-white font-medium font-martian-mono -ml-3"}>ikilinks</span>
         </div>
     );
 };
