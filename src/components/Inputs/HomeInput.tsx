e25f0732062f9376c58ae48c9f56b1a4dbd2b694
Galaktik-hub <dvatheking@gmail.com>
2025-02-26 19:21:38 +0100

    Changed the color of input to white

--- src/components/Inputs/HomeInput.tsx
+++ src/components/Inputs/HomeInput.tsx
@@ -16,7 +16,7 @@ export const HomeInput: React.FC<HomeInputProps> = ({
     return (
         <input
             type="text"
-            className="gap-2.5 self-stretch px-2.5 w-full text-base text-gray-400 bg-gray-700 rounded-lg min-h-12 focus:outline-none focus:shadow-[0_0_0_3px_rgba(29,151,216,1)]"
+            className="gap-2.5 self-stretch px-2.5 w-full text-base text-white bg-gray-700 rounded-lg min-h-12 focus:outline-none focus:shadow-[0_0_0_3px_rgba(29,151,216,1)]"
             placeholder={placeholder}
             value={value}
             onChange={(e) => onChange(e.target.value)}
