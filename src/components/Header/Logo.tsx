d0fdc384a3177363f47f198ae7616d20851f0144
Kirushield <elankeethan@edu.univ-eiffel.fr>
2025-02-26 20:25:57 +0100

    [+] Update logo with precise size

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -3,9 +3,9 @@ import logo from '../../assets/logo.png';
 
 const Logo: React.FC = () => {
     return (
-        <div className={"flex items-end justify-center leading-none"}>
-            <img src={logo} alt="W" className={"h-10 w-auto"} />
-            <span className={"text-xl text-white font-medium font-martian-mono"}>ikilinks</span>
+        <div className={"flex items-center justify-center leading-none"}>
+            <img src={logo} alt="W" className={"h-[50px] w-auto -mb-2"} />
+            <span className={"text-base self-end text-white font-medium font-martian-mono -ml-2"}>ikilinks</span>
         </div>
     );
 };
