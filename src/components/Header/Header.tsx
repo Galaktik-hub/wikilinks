3488310380527366967f91c7af5084886f84e635
Kirushield <elankeethan@edu.univ-eiffel.fr>
2025-02-26 19:55:25 +0100

    [+] Add font : Martian mono reusable [+] Update logo, placed at the center

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,9 @@ import Logo from './Logo';
 
 const Header = () => {
     return (
-        <header className={"w-full px-6 py-4 bg-sea-salt relative"}>
+        <header className={"w-full mt-6 px-6 py-4 bg-sea-salt relative flex justify-center"}>
             <div className={"max-w-7xl mx-auto flex justify-between items-center"}>
-                <Logo text={"ikiLinks"}/>
+                <Logo />
             </div>
         </header>
     );
