abc8567f6fff46be627e8efa251c2850b40be521
JulienS-Code <julien.synaeve@gmail.com>
2025-02-27 19:47:38 +0100

    WIK-5: Page d'attente/jeu - InventaryButton with timeout to display text during 10s

---
+++ src/components/Buttons/InventaryButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import * as React from "react";
+import InventarySVG from "../../assets/InventarySVG.tsx";
+import {useEffect, useState} from "react";
+
+const IventaryButton: React.FC<{ onClick?: () => void; disabled?: boolean }> = ({ onClick, disabled = false }) => {
+    const [showText, setShowText] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setShowText(false), 10000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <button
+            onClick={onClick}
+            disabled={disabled}
+            className="inline-flex gap-2 items-center p-4 bg-blue-700 rounded-lg cursor-pointer h-[50px]"
+        >
+            <InventarySVG />
+            {showText && <span className="text-base text-white max-md:text-base max-sm:text-sm font-['Inter']">
+                Inventaire
+            </span>}
+        </button>
+    );
+};
+
+export default IventaryButton;
