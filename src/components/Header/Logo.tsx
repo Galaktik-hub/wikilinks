e2267d75a7410eb08fa82cf8450d0e3045e8f9aa
JulienS-Code <julien.synaeve@gmail.com>
2025-03-01 17:14:57 +0100

    WIK-4: Page d'accueil - Old logo image crop - Header size corrected - Remove unnecessary images

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from '../../assets/Logo/logo.png';
+import logo from '/wikilinks_logo.png';
 
 const Logo: React.FC = () => {
     return (
         <div className={"flex items-center justify-center leading-none"}>
-            <img src={logo} alt="W" className={"h-[60px] w-auto -mb-2"} />
-            <span className={"text-lg self-end text-white font-medium font-martian-mono -ml-3"}>ikilinks</span>
+            <img src={logo} alt="W" className={"h-[45px] w-auto"} />
+            <span className={"text-xl self-end text-white font-medium font-martian-mono"}>ikilinks</span>
         </div>
     );
 };
