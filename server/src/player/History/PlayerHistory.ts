8ad2767b3d4a043b5771bc86f16c3c0f7a9769c1
JulienS-Code <julien.synaeve@gmail.com>
2025-03-29 18:43:56 +0100

    WIK-22: Initialisation de la partie - Created PlayerHistory class and added into Player

---
+++ server/src/player/History/PlayerHistory.ts
@@ -0,0 +1,25 @@
+import {HistoryStep, HistoryType} from "./PlayerHistoryProps";
+import {Player} from "../player";
+
+export class PlayerHistory {
+    private steps: HistoryStep[] = [];
+
+    constructor(private player: Player) {
+        // Enregistrement de l'action de démarrage dès la création de l'historique
+        this.addStep('start');
+    }
+
+    // Ajoute une étape à l'historique, en injectant automatiquement le nom du joueur dans les données
+    addStep(type: HistoryType, data?: Record<string, string>): void {
+        const step: HistoryStep = {
+            type,
+            data: { player: this.player.name, ...data },
+            date: new Date()
+        };
+        this.steps.push(step);
+    }
+
+    getHistory(): HistoryStep[] {
+        return this.steps;
+    }
+}
--- server/src/player/player.ts
+++ server/src/player/player.ts
@@ -1,17 +1,20 @@
 import {Inventory} from "./Inventory/Inventory";
 import {ArtifactName} from "./Inventory/InventoryProps";
+import {PlayerHistory} from "./History/PlayerHistory";
 
 export class Player {
     id: string;
     name: string;
     isLeader: boolean;
     inventory: Inventory;
+    history: PlayerHistory;
 
     constructor(name: string, isLeader: boolean = false) {
         this.id = crypto.randomUUID();
         this.name = name;
         this.isLeader = isLeader;
         this.inventory = new Inventory();
+        this.history = new PlayerHistory(this);
     }
 
     equals(other: Player): boolean {
@@ -24,6 +27,7 @@ export class Player {
 
     addArtifact(name: ArtifactName, quantity: number = 1) {
         this.inventory.addArtifact(name, quantity);
+        this.history.addStep('foundArtifact', { artefact: name });
     }
 
     removeArtifact(name: ArtifactName, quantity: number = 1) {
@@ -31,6 +35,11 @@ export class Player {
     }
 
     useArtifact(name: ArtifactName): boolean {
-        return this.inventory.useArtifact(name);
+        const result = this.inventory.useArtifact(name);
+        if (result) {
+            // Enregistrement de l'action d'utilisation d'un artefact
+            this.history.addStep('usedArtifact', { artefact: name });
+        }
+        return result;
     }
 }
