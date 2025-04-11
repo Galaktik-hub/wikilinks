03ac8ca221731c5339af447b3ca02d3227df9434
JulienS-Code <julien.synaeve@gmail.com>
2025-04-11 19:10:14 +0200

    WIK-50: Dynamiser la page Waiting Room - Blocks access to the MoreOptionsIcon for the current user

--- src/components/Sections/WaitingRoom/Player/PlayerList.tsx
+++ src/components/Sections/WaitingRoom/Player/PlayerList.tsx
@@ -6,16 +6,17 @@ import {PlayerCard} from "./PlayerCard.tsx";
 type PlayerListProps = {
     isHost: boolean;
     players: {username: string; role: string}[];
+    currentUsername?: string | null;
 };
 
-export const PlayerList: React.FC<PlayerListProps> = ({isHost, players}) => (
+export const PlayerList: React.FC<PlayerListProps> = ({isHost, players, currentUsername}) => (
     <div className="card-container whitespace-nowrap">
         <div className="w-full flex justify-start">
             <h2 className="blue-title-effect">Joueurs</h2>
         </div>
         <div className="mt-4 w-full text-base leading-none text-white flex flex-wrap justify-center gap-4">
             {players.map(player => (
-                <PlayerCard key={player.username} playerName={player.username} isPlayerAdmin={player.role === "creator"} isHost={isHost} />
+                <PlayerCard key={player.username} playerName={player.username} isPlayerAdmin={player.role === "creator"} isHost={isHost} currentUsername={currentUsername} />
             ))}
         </div>
     </div>
