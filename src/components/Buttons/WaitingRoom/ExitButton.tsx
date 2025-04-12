4b94d7b3bee03297462dfc041941ae04840e38f7
Galaktik-hub <dvatheking@gmail.com>
2025-04-12 10:18:33 +0200

    Exit buttons working

--- src/components/Buttons/WaitingRoom/ExitButton.tsx
+++ src/components/Buttons/WaitingRoom/ExitButton.tsx
@@ -1,24 +1,31 @@
 import * as React from "react";
 import ExitSVG from "../../../assets/WaitingRoom/ExitSVG.tsx";
 import MainButton from "../MainButton.tsx";
+import {SocketContext} from "../../../context/SocketContext.tsx";
+import {useCallback} from "react";
 
 type DeleteButtonProps = {
     isHost: boolean;
     onClick?: () => void;
 };
 
-const ExitButton: React.FC<DeleteButtonProps> = ({isHost, onClick}) => {
-    const handleClick = React.useCallback(() => {
-        console.log("ExitButton clicked");
-        if (onClick) {
-            onClick();
+const ExitButton: React.FC<DeleteButtonProps> = ({isHost}) => {
+    const socket = React.useContext(SocketContext);
+
+    const handleClick = useCallback(() => {
+        if (isHost) {
+            socket?.sendMessageToServer({ kind: "close_room" });
+            window.location.href = "/";
+        } else {
+            socket?.sendMessageToServer({ kind: "disconnect" });
+            window.location.href = "/";
         }
-    }, [onClick]);
+    }, [isHost, socket]);
 
     return (
         <MainButton color="220, 38, 38" className="bg-red-600" ariaLabel={isHost ? "Supprimer" : "Quitter"} onClick={handleClick}>
             <span className="text-center text-xl">{isHost ? "Supprimer" : "Quitter"}</span>
-            {!isHost ? <ExitSVG className="ml-2 w-6 h-6 text-white" /> : null}
+            <ExitSVG className="ml-2 w-6 h-6 text-white" />
         </MainButton>
     );
 };
