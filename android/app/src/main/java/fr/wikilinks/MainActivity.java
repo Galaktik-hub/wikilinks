e510ce03aad3f0e133afb69b7d6164c8c8f37b70
Galaktik-hub <dvatheking@gmail.com>
2025-04-16 15:03:15 +0200

    Base WebView app

---
+++ android/app/src/main/java/fr/wikilinks/MainActivity.java
@@ -0,0 +1,43 @@
+package fr.wikilinks;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    class JavaScriptObject {
+        @JavascriptInterface
+        public void hello() {
+            Log.println(Log.INFO, "[WikiLinks]", "Hello !");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Setting up the WebView
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+
+        webView.addJavascriptInterface(new JavaScriptObject(), "Android");
+        webView.loadUrl("https://192.168.1.38:5173/");
+    }
+}
