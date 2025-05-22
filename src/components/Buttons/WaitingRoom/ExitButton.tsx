083fa5e8e2f5752e5abd424b3c1097b503388fe4
synaeve <synaeve@idmgroup.com>
2025-05-22 15:45:38 +0200

    WIK-59: Pop-up de confirmation - Added openModal before confirm ExitButton action

--- src/components/Buttons/WaitingRoom/ExitButton.tsx
+++ src/components/Buttons/WaitingRoom/ExitButton.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import ExitSVG from "../../../assets/WaitingRoom/ExitSVG.tsx";
 import MainButton from "../MainButton.tsx";
-import {useCallback} from "react";
 import {useWebSocket} from "../../../context/WebSocketContext.tsx";
 import {useNavigate} from "react-router-dom";
 import {usePlayersContext} from "../../../context/PlayersContext";
+import {useModalContext} from "../../Modals/ModalProvider";
 
 type DeleteButtonProps = {
     isHost: boolean;
@@ -15,8 +15,32 @@ const ExitButton: React.FC<DeleteButtonProps> = (props: DeleteButtonProps) => {
     const socketContext = useWebSocket();
     const playerContext = usePlayersContext();
     const navigate = useNavigate();
+    const {openModal, closeModal} = useModalContext();
 
-    const handleClick = useCallback(() => {
+    const title = isHost ? "Supprimer le salon" : "Quitter le salon";
+    const message = isHost
+        ? "Voulez-vous vraiment supprimer le salon ? Tous les joueurs seront redirigés vers l'accueil."
+        : "Voulez-vous vraiment quitter le salon ?";
+
+    const handleClick = () => {
+        openModal({
+            title: title,
+            type: "confirmation",
+            content: {
+                message: message,
+                cancelButton: {label: "Annuler", onClick: () => closeModal()},
+                okButton: {
+                    label: "Confirmer",
+                    onClick: () => {
+                        handleConfirm();
+                        closeModal();
+                    },
+                },
+            },
+        });
+    };
+
+    const handleConfirm = () => {
         if (isHost) {
             socketContext.send({kind: "close_room"});
         } else {
@@ -24,7 +48,7 @@ const ExitButton: React.FC<DeleteButtonProps> = (props: DeleteButtonProps) => {
         }
         playerContext.exit();
         navigate("/");
-    }, [isHost, navigate, socketContext, playerContext]);
+    }
 
     return (
         <MainButton color="220, 38, 38" className="bg-red-600" ariaLabel={isHost ? "Supprimer" : "Quitter"} onClick={handleClick}>
