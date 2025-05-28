dad57a4a93f6babefd710b57b060c8d9e4b2f7a7
synaeve <synaeve@idmgroup.com>
2025-05-28 16:04:32 +0200

    WIK-40: Déploiement - Added script to copy solver assets after build - Updated inventory to init stackableArtifact to 1 item each - Updated artifactKeyWord to length 8

---
+++ server/src/scripts/copySolverAssets.ts
@@ -0,0 +1,15 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const files = ['wikiSolveur', 'wikiSolveur.exe', 'solver/graph.txt', 'solver/graph.txt.bin'];
+const destBase = path.join(__dirname, '../../build/server/src/solver');
+
+if (!fs.existsSync(destBase)) fs.mkdirSync(destBase, { recursive: true });
+
+for (const file of files) {
+    const src = path.join(__dirname, '../solver', file);
+    const dest = path.join(destBase, file);
+    const destDir = path.dirname(dest);
+    if (!fs.existsSync(destDir)) fs.mkdirSync(destDir, { recursive: true });
+    fs.copyFileSync(src, dest);
+}
